3bc14e5ccf646d02f6a2589ef91ecdf4dd0eb97b
helmi.agustiawan <helmi@bitcoin.co.id>
2024-05-07 15:27:39 +0700

    implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,39 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const loggedIn = {
+    firstName: "Helmi",
+    lastName: "Agustiawan",
+    email: "helmiagustiawan905@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={"Guest"}
+            subtext={
+              "Access and manage your account and transaction efficiently."
+            }
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+    </section>
+  );
 };
 
 export default Dashboard;
